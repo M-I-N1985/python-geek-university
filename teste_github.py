0aad368754b34957b83dbf5372704a9444358640
Iuri Nunes <iuri.mnunes@gmail.com>
2023-02-10 13:42:57 -0300

    Enviado primeiro arquivo de um pc local

---
+++ teste_github.py
@@ -0,0 +1,12 @@
+"""
+Envindo um documento deste repositorio local.
+O que foi feito:
+- git init
+- git clone url
+- git user.name/email
+- criado um arquivo
+- git status
+- git add arquivo
+- git commit -m "o que foi feito"
+- git push -u origin main
+"""
